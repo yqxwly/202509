b5b08559d4cecdea4b0ac442552f4d1444f9c5a4
pengzy <pengzy@igrslab.com>
2026-07-14 17:12:11 +0800

    add instanceof

--- JS/原型链.js
+++ JS/原型链.js
@@ -29,3 +29,14 @@ console.log(teacher.hasOwnProperty('name')); // true
 console.log(teacher.hasOwnProperty('teach')); // false
 
 console.log(teacher.__proto__ === Teacher.prototype); // true
+
+// typeof 只能判断基本数据类型，不能判断对象的具体类型
+console.log(typeof []); // object
+
+// instanceof 判断对象是否是某个类的实例，只有在原型链上存在就会返回 true
+console.log([] instanceof Array); // true
+console.log([] instanceof Object); // true，除了基本数据类型，其他都是对象，所有对象都继承自 Object.prototype
+
+console.log(teacher instanceof Teacher); // true
+console.log(teacher instanceof Person); // true
+console.log(teacher instanceof Object); // true
